9248ac407a01b394825d1dce2b8eadcec774d3aa
Dan Pomeranz <dan.pomeranz@tulip.co>
2023-02-28 09:22:23 -0500

    test

--- test/schema.test.py
+++ test/schema.test.py
@@ -199,7 +199,7 @@ class SchemaTest(unittest.TestCase):
     def test_drop_special_char_columns(self):
         schema1 = Schema()
         schema1.read_object({"abc ": 1, "def@#": 1, "$$ghi": 1, "jkl": 1, "!@#mno": 1})
-        print(schema1)
+        print(schema1.schema)
         self.assertEqual(3, schema1.drop_special_char_columns())
         self.assertEqual(schema1.schema, {"abc ": 1, "jkl": 1})
 
@@ -207,6 +207,7 @@ class SchemaTest(unittest.TestCase):
         schema1 = Schema()
         schema1.read_object(CASE_1)
         schema1.read_object(CASE_1)
+        print(schema1.schema)
         self.assertEqual(4, schema1.drop_duplicate_columns())
 
     def test_drop_duplicate_columns2(self):
