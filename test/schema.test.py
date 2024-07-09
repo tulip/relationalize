dc4242b167a43f5d53d0b75c25ca2928ea2fed1a
Malte <m1a9l9t7e@gmail.com>
2024-07-09 18:09:36 +0100

    Ignore spacing and linebreaks when comparing DDL

--- test/schema.test.py
+++ test/schema.test.py
@@ -1,3 +1,4 @@
+import re
 import unittest
 from copy import deepcopy
 
@@ -121,14 +122,14 @@ class SchemaTest(unittest.TestCase):
     def test_generate_ddl_no_choice(self):
         schema1 = Schema()
         schema1.read_object(CASE_1)
-        self.assertEqual(CASE_1_DDL, schema1.generate_ddl("test"))
+        self.assertEqual(re.sub(r'\s+', ' ', CASE_1_DDL), re.sub(r'\s+', ' ', schema1.generate_ddl("test")))
 
     def test_generate_ddl_choice(self):
         schema1 = Schema()
         schema1.read_object(CASE_1)
         schema1.read_object(CASE_2)
 
-        self.assertEqual(CASE_2_DDL, schema1.generate_ddl("test"))
+        self.assertEqual(re.sub(r'\s+', ' ', CASE_2_DDL), re.sub(r'\s+', ' ', schema1.generate_ddl("test")))
 
     def test_none_cases(self):
         schema1 = Schema()
